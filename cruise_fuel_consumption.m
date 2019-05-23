f762fe98ab9f3476732a89c44a9e00420146ed80
Emircan Kılıçkaya <emircankilickaya97@gmail.com>
2019-05-23 17:31:27 +0300

    Final

--- cruise_fuel_consumption.m
+++ cruise_fuel_consumption.m
@@ -9,11 +9,11 @@ surf=427.82; %Surface
 %cruise =cruise_speed_schedule(H); %Taking the speed of cruise in altitude that we are on.
 %cruise=Cas_to_Tas(cruise*0.514444444,H*0.3048); %Converting to TAS but careful with units
 cl=cl_calc(H*0.3048,cruise,0,m);
-tas=cruise*1; 
+tas=cruise*1.9438; 
 cd0_cr=0.022913;
 cd2_cr=0.042347;
 cd=cd_calc(cl,cd0_cr,cd2_cr);
-Thrust=cd*rho*(tas^2)*surf*0.5/1000; %Thrust=Drag 
+Thrust=cd*rho*(cruise^2)*surf*0.5/1000; %Thrust=Drag 
 nu=Cf_1*(1+(tas/Cf_2));
 f=Thrust*Cf_cr*nu;
 end
